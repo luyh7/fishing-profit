27552565fa21ea093e12147f0b846599ef5f80b5
whisper <470103427@qq.com>
2026-05-10 21:56:23 +0800

    feat: Refactor panel head actions layout to use grid for better responsiveness

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.FISH_FISHING_CONFIG = {
   versionPrefix: "1.1",
-  gitCommitCount: 18,
+  gitCommitCount: 19,
   nestBuffSourceUrl:
     "https://raw.githubusercontent.com/luyh7/fishing-profit/gh-pages/nest-buff.json",
   baseIntervalHours: 1,
